718e532ddce135eabea2d4973084bce63ff2bc6c
PoojaSoni30 <pooja.soni@paytm.com>
2022-04-25 12:07:24 +0530

    Add comment

--- WalE/WalE/ViewModel/AstronomyVM.swift
+++ WalE/WalE/ViewModel/AstronomyVM.swift
@@ -72,7 +72,7 @@ class AstronomyViewModel {
         let queue = DispatchQueue(label: "Network")
         monitor.start(queue: queue)
     }
-    //to check whether the user is entering for the first time a day 
+    //to check whether the user is entering for the first time a day
     func isUserEnteredFirstTimeForTheDay() -> Bool {
         var isFirstTimeUser: Bool = false
         if let currentDate = defaults.object(forKey: "currentDate") as? Date {
