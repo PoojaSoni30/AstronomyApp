6f51e2564baa3ec635df8554e06a6d234a934b1a
PoojaSoni30 <pooja.soni@paytm.com>
2022-04-25 10:49:05 +0530

    Pushing /main StoryBoard created a UI
    T show the UI, Simple. reation of UI has been pushed

---
+++ WalE/WalE/View/AstronomyViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AstronomyViewController.swift
+//  SampleApp
+//
+//  Created by Pooja Soni on 24/04/22.
+//
+
+import UIKit
+
+class AstronomyViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var descriptionlbl: UILabel!
+    @IBOutlet weak var loadinglbl: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
+
