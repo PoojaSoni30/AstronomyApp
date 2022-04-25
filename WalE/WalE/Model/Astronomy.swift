5e340594976e47634336b385751201d7134d14a7
PoojaSoni30 <pooja.soni@paytm.com>
2022-04-25 10:56:41 +0530

    Model creation
    Model creation for the json

---
+++ WalE/WalE/Model/Astronomy.swift
@@ -0,0 +1,38 @@
+//
+//  Astronomy.swift
+//
+//  Created by Pooja Soni on 23/04/22.
+//
+
+import Foundation
+
+
+struct Astronomy: Codable, Equatable{
+    /// The title of the astronomy
+    let title: String
+
+    /// The description of the astronomy of the date
+    let description: String
+
+    /// The image URL the post lives at.
+    let imageURL: URL?
+    
+    /// The hd image URL the post lives at.
+    let imageHDURL: URL
+    
+    /// The type of the media, by default keeping it as image only
+    let mediaType: String
+    
+    ///date of the astronomy
+    let date: String
+    
+    
+    enum CodingKeys: String, CodingKey {
+        case title = "title"
+        case description = "explanation"
+        case imageURL = "url"
+        case imageHDURL = "hdurl"
+        case date = "date"
+        case mediaType = "media_type"
+    }
+}
